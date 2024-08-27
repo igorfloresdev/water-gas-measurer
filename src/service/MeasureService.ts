e96f610d801bfe9ced92cea30d76673688d057bc
Igor Flores <dev.igorflores@gmail.com>
2024-08-27 16:23:29 -0300

    Refactored MeasureService

--- src/service/MeasureService.ts
+++ src/service/MeasureService.ts
@@ -1,16 +1,8 @@
 import { IMeasure } from '../interface/IMeasure'
-import { GoogleAIFileManager } from '@google/generative-ai/server'
-
-const fileManager = new GoogleAIFileManager(process.env.API_KEY as string)
+import { fileManager } from '../lib/fileManager'
 
 export class MeasureService {
   static async create(data: IMeasure) {
-    const metadata = {
-      mimeType: 'image/jpeg',
-      displayName: `measure_${data.customer_code}_${Date()}`,
-    }
-
-    const file = await fileManager.uploadFile(data.image, metadata)
-    console.log(file)
+    const file = await fileManager(data.image, data.customer_code)
   }
 }
