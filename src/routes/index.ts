259726ade5cc1e8d03bf81e8417be6696e853fee
Igor Flores <dev.igorflores@gmail.com>
2024-08-27 15:50:47 -0300

    Increased a limit in express.json to request strings and included a MeasureController route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express'
+import { MeasureController } from '../controller/MeasureController'
 
 const routes = (app: Application) => {
   app.route('/').get((req: Request, res: Response) => {
     res.status(200).send('Water and Gás Measurer API Developed By igorfloresdev 🚀')
   })
 
-  app.use(express.json())
+  app.use(express.json({ limit: '100mb' }))
+
+  app.post('/upload', MeasureController.create)
 }
 
 export default routes
