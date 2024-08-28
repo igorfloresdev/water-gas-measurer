d16189c1511d490642b3beb684c187a82be3b075
igorfloresdev <dev.igorflores@gmail.co>
2024-08-27 23:03:26 -0300

    Created MeasureService

--- src/service/MeasureService.ts
+++ src/service/MeasureService.ts
@@ -1,8 +1,25 @@
-import { IMeasure } from '../interface/IMeasure'
 import { fileManager } from '../lib/fileManager'
+import { generateContext } from '../lib/generateContext'
+import { MeasureRepository } from '../repository/MeasureRepository'
 
 export class MeasureService {
-  static async create(data: IMeasure) {
-    const file = await fileManager(data.image, data.customer_code)
+  static async create(image: string, customer_code: string, measure_type: 'WATER' | 'GAS', measure_datetime: string) {
+    const { file } = await fileManager(image, customer_code)
+
+    const result = await generateContext(file.mimeType, file.uri, measure_type)
+
+    console.log(result)
+
+    const measureData = {
+      customer_code,
+      measure_datetime,
+      measure_type,
+      image_url: file.uri,
+      measure_value: parseInt(result.value),
+    }
+
+    const createdMeasure = await MeasureRepository.create(measureData)
+
+    return createdMeasure
   }
 }
