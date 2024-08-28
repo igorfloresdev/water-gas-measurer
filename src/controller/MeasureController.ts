4b2cb464a8424f5472882b806922fc030ee0a278
igorfloresdev <dev.igorflores@gmail.co>
2024-08-27 23:01:57 -0300

    Created MeasureController

--- src/controller/MeasureController.ts
+++ src/controller/MeasureController.ts
@@ -10,11 +10,20 @@ export class MeasureController {
       const request = req.body
       request.measure_type = measure_type.toUpperCase()
 
-      const data = MeasureSchema.parse(req.body)
+      const data = MeasureSchema.parse(request)
 
-      const createdMeasure = await MeasureService.create(data)
+      const createdMeasure = await MeasureService.create(
+        data.image,
+        data.customer_code,
+        data.measure_type,
+        data.measure_datetime
+      )
 
-      res.status(200).json(createdMeasure)
+      res.status(200).json({
+        image_url: createdMeasure.imageUrl,
+        measure_value: createdMeasure.measureValue,
+        measure_uuid: createdMeasure.measureUuid,
+      })
     } catch (error) {
       next(error)
     }
